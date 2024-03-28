2489c5ae546e24012cc743eb0c3fe48467852611
brendan smith <smit70592@icloud.com>
2024-03-28 09:32:31 -0500

    Puppy data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <p>hey</p>;
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("featuredPup: ", featuredPup);
+  return (
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
